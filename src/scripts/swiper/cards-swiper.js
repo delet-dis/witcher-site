be2972316cac9b0328d816c1458e8dad36eca4eb
Igor <efimov.036@gmail.com>
2020-06-23 12:48:41 +0700

    swiper done

--- src/scripts/swiper/cards-swiper.js
+++ src/scripts/swiper/cards-swiper.js
@@ -1,6 +1,11 @@
 $(document).ready(function () {
   //initialize swiper when document ready
   var mySwiper = new Swiper ('.swiper-container', {
-    loop: true
+    loop: true,
+    slidesPerView:2,
+    spaceBetween:25,
+    navigation: {
+      nextEl: '.arrow'
+    },
   })
 });
