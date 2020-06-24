4b3116edf9b143035277ac3283dcdd4505886583
Igor <efimov.036@gmail.com>
2020-06-24 13:13:22 +0700

    adaptive done

--- src/scripts/swiper/cards-swiper.js
+++ src/scripts/swiper/cards-swiper.js
@@ -2,10 +2,15 @@ $(document).ready(function () {
   //initialize swiper when document ready
   var mySwiper = new Swiper ('.swiper-container', {
     loop: true,
-    slidesPerView:2,
-    spaceBetween:25,
+    slidesPerView:1,
     navigation: {
       nextEl: '.arrow'
     },
+    breakpoints:{
+      540:{
+        slidesPerView:2,
+        
+      }
+    }
   })
 });
