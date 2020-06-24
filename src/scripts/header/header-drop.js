8bc611eae4ed75f26246211a297b9b07cb3eeea7
Igor <efimov.036@gmail.com>
2020-06-24 13:09:04 +0700

    added little animation for menu icon

--- src/scripts/header/header-drop.js
+++ src/scripts/header/header-drop.js
@@ -1,6 +1,6 @@
 var menuButton = document.querySelector('.menu-button');
 var menu = document.querySelector('.header');
 menuButton.addEventListener('click', function(){
-  console.log('Клик по кнопке');
+  menuButton.classList.toggle('menu-button-active');
   menu.classList.toggle('header-active');
 }) 
