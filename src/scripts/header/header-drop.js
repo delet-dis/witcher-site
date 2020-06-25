1cca401d5f0a852dd413c36ec8e9b224d3acce1f
Igor <efimov.036@gmail.com>
2020-06-25 12:25:41 +0700

    fixed animations

--- src/scripts/header/header-drop.js
+++ src/scripts/header/header-drop.js
@@ -3,4 +3,5 @@ var menu = document.querySelector('.header');
 menuButton.addEventListener('click', function(){
   menuButton.classList.toggle('menu-button-active');
   menu.classList.toggle('header-active');
+  menu.classList.toggle('animate__fadeInDown');
 }) 
